dda85262956138a4f6d06194aa114d4f40d70477
vcbyel <byel7382@gmail.com>
2025-05-25 17:22:57 +0000

    desafio mestre xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
 
+// void eu defino a formula a ser executada mesmo sem ela apresentar valor de execução!
+void movertorre(int casa) { //definindo movimento
+	if (casa > 0) {
+		printf("Direita\n");//printando o lado da casa
+		movertorre(casa - 1);
+	}
+	printf("\n");
+}
+
+void moverrainha(int casa) { //definindo movimento
+   if (casa > 0) {
+   
+	printf("Esquerda\n");//printando o lado da casa
+	moverrainha(casa - 1);
+}
+printf("\n");
+}
+
+void moverbispo(int casa) {
+   if (casa > 0) {
+   
+    printf("Cima\n");//printando o lado da casa
+	printf("Direita\n");//printando o lado da casa
+	moverbispo(casa - 1);
+}
+printf("\n");
+}
+
+
 int main () {
-	//mover torre 5 casas para a direita
-int torre, bispo, rainha, cavalo, movimentocavalo = 1;
-int option;
+	int cavalo, option, movimentocavalo = 1;// declarando a variaveis do cavalo.
 
 
 printf("Qual peça deseja mover?\n");
@@ -14,34 +41,17 @@ printf("Qual peça deseja mover?\n");
         printf("5. Sair\n");
         printf("Escolha uma opção: \n");
         scanf("%d", &option);
-
-switch (option) {
-
-
-case 1: for (torre = 0; torre < 5; torre++){
-	printf("Direita\n"); //imprime a direção do movimento
+        
+        switch(option) {
 	
-}	
-    break;
-	//mover rainha 8 casas para esquerda
-case 2:	while (rainha < 8)
-	{
-		printf("Esquerda\n");// imprime a direção da rainha
-		rainha++;
-	}
-	break;
-	//movendo bispo
-case 3:	do 
-	{
-		printf("Cima\n");
-		printf("Direita\n");
-		bispo++;
-	} while (bispo < 6);
-	break;
-	
-	//movendo cavalo
-	
-case 4:	while (movimentocavalo--)
+case 1: movertorre(5);//mover torre 5 casas para a direita 	
+//aqui eu declaro o valor, não no void, é aqui!!
+break;
+case 2: moverrainha(8); //mover rainha 8 casas para esquerda
+break;
+case 3: moverbispo(5); //movendo bispo
+break;	
+case 4: while (movimentocavalo--)
 	{
 		for (cavalo = 0; cavalo < 2; cavalo++) {
 			printf("Baixo\n"); // imprime cima duas vezes
@@ -50,15 +60,11 @@ case 4:	while (movimentocavalo--)
 		
 		printf("\n");
 	}
-    break;
-//saindo
+	break;
 case 5:
 printf("saindo..");	
 	break;
-	
 }
-	
-	
 	return 0;
 	
 }
